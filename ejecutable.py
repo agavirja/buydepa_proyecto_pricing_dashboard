51014c81ab60082d0583ecf40e3f547709fc5e4c
agavirja <gaviriaalejandro@hotmail.com>
2022-12-20 19:18:04 -0500

    actualizar cache

--- ejecutable.py
+++ ejecutable.py
@@ -47,7 +47,7 @@ dataexportsimilares = pd.DataFrame()
 
 idcontinue = True
 
-@st.cache(allow_output_mutation=True)
+@st.cache
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits, changeinput=0):
     return ''.join(random.choice(chars) for _ in range(size))
 
